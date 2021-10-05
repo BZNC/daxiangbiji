439cc862d9adf50722bdcd90cb476bb69801bb6b
leo <leo.liuxin@foxmail.com>
2021-10-05 16:56:46 +0800

    add new file

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import login from '@/components/login'
+import NotebooksList from '@/components/NotebooksList'
+import NoteDetail from '@/components/NoteDetail'
+import TrashDetail from '@/components/TrashDetail'
 
 Vue.use(Router)
 
@@ -9,13 +12,27 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/login'
     },
     {
-      path:'/login',
-      name:'login',
-      component:login
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/notebooks',
+      name: 'notebooks',
+      component: NotebooksList.vue
+    },
+    {
+      path: '/note/:noteId',
+      name: 'noteID',
+      component: NoteDetail
+    },
+    {
+      path: '/trash/:noteId',
+      name: 'trash',
+      component: TrashDetail.vue
     }
   ]
 })
